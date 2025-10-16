39e54e2291551bb915082a874b699f443de9dd4c
Clare Shave <cscoaching300@gmail.com>
2025-10-16 17:48:26 +0100

    Fix Render build: align client deps and use npm install

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,7 @@
-// client/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
-  build: {
-    outDir: 'dist'
-  }
+  build: { outDir: 'dist' }
 })
